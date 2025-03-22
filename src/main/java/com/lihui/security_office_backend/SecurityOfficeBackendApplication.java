0513d810c721f0ddf5e7c88a886df79bbe136796
lihuibear <lihuibear4@gmail.com>
2025-03-22 11:26:28 +0800

    add:安全教育后端初始化

---
+++ src/main/java/com/lihui/security_office_backend/SecurityOfficeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lihui.security_office_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOfficeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityOfficeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lihui/security_office_backend/SecurityOfficeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lihui.security_office_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOfficeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
